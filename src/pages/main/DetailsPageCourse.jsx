a5ced350d64e9c1263d4e20f5a381f51cd54334b
Elihoang <129831728+Elihoang@users.noreply.github.com>
2025-04-06 14:15:25 +0700

    git push admin order

--- src/pages/main/DetailsPageCourse.jsx
+++ src/pages/main/DetailsPageCourse.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { Play, ArrowRight, ChevronDown, ChevronUp, ArrowLeft, Clock, Calendar } from 'lucide-react';
 import { FaPlayCircle } from 'react-icons/fa';
-import { getLessonsByCourseId, getLessonById } from '@/api/lessionApi';
+import { getLessonsByCourseId, getLessonById } from '@/api/lessonApi';
 import { jwtDecode } from 'jwt-decode';
 import YouTube from 'react-youtube';
 import { toast } from "react-toastify";
